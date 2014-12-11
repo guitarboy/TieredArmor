d6147558aa06eb5c9e379f945430a2d7c3baeade
guitarboy <pelimies5352@gmail.com>
2014-12-11 16:15:49 +0200

    First commit

--- src/main/java/net/jumpingjaguar/tieredarmor/mob/EntityGabara.java
+++ src/main/java/net/jumpingjaguar/tieredarmor/mob/EntityGabara.java
@@ -1,10 +1,10 @@
 package net.jumpingjaguar.tieredarmor.mob;
 
 import net.jumpingjaguar.tieredarmor.init.ModItems;
-import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
-import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.*;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.world.World;
 
@@ -14,7 +14,13 @@ public class EntityGabara extends EntityMob{
         super(par1World);
         this.setSize(3.0F, 5.0F);
         this.tasks.addTask(0, new EntityAIWander(this, 0.7D));
-        this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+        this.tasks.addTask(1, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.0D, false));
+        this.tasks.addTask(3, new EntityAIMoveTowardsRestriction(this, 1.0D));
+        this.tasks.addTask(4, new EntityAILookIdle(this));
+        this.tasks.addTask(5, new EntityAITempt(this, 1.0D, Items.wheat_seeds, true));
+        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
     }
 
     protected Item getDropItem() {
@@ -24,5 +30,17 @@ public class EntityGabara extends EntityMob{
     public boolean isAIEnabled() {
         return true;
     }
+
+    /*protected String getLivingSound() {
+        return Reference.MOD_ID + ":mob.gabara.say";
+    }
+
+    protected String getHurtSound() {
+        return Reference.MOD_ID + ":mob.gabara.hurt";
+    }
+
+    protected String getDeathSound() {
+        return Reference.MOD_ID + ":mob.gabara.death";
+    }*/
 
 }
